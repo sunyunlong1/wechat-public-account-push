f8b8f5194db4692dd5279892bcd49b98493a5d72
sunyunlong1 <45194769+sunyunlong1@users.noreply.github.com>
2023-02-12 21:40:10 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oMiFh53TBSMc1F5IDlrTWhUK0xqk',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'yOJdIgH_3zJR8c3BtJFSz-4SDB9nQpdgVpPMYLdAuw0',
+      useTemplateId: 'A-D9GWXa42TZ2S3Tm2mhqbHuB_3MEHV9I8RK_yeLhfk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-02',
       festivals: [
